2fc6f15d5931d9a426a7c9286fd2c8639b66da68
Kreal11 <kirillshyrokov2@gmail.com>
2023-11-10 17:12:35 +0200

    added persistor for token and started login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export const Login = () => {
-  return <div>Login</div>;
+  const { register, handleSubmit, reset } = useForm;
+
+  return (
+    <form action="">
+      <input type="text" />
+      <input type="text" />
+    </form>
+  );
 };
