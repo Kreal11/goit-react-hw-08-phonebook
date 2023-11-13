611fc1c0eb240cf351bb6c4a8a281fe7535757de
Kreal11 <kirillshyrokov2@gmail.com>
2023-11-13 15:49:07 +0200

    done notFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -6,8 +6,9 @@ import { NavLink } from 'react-router-dom';
 export const NotFound = () => {
   return (
     <ImgWrapper>
+      <h2>It seems like You can find nothing here😭</h2>
       <span>
-        You can go<NavLink to="/">home</NavLink>
+        You can go <NavLink to="/">back</NavLink>
       </span>
     </ImgWrapper>
   );
--- src/pages/NotFound/StyledNotFound.js
+++ src/pages/NotFound/StyledNotFound.js
@@ -8,10 +8,28 @@ export const ImgWrapper = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-`;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 22px;
+  text-align: center;
 
-// export const StyledImg = styled.img`
-//   width: 100vw;
-//   height: 100vh;
-//   aspect-ratio: inherit;
-// `;
+  h2 {
+    margin-top: 50px;
+  }
+
+  span {
+    min-width: 200px;
+    a {
+      padding: 2px;
+      color: white;
+      transition: color 0.5s, background-color 0.5s;
+      border-radius: 3px;
+      &:hover {
+        background-color: white;
+        color: #573b4a;
+      }
+    }
+  }
+`;
